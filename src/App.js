744422b57571a1a41533ee68b68dcf923cbb36fe
HybridTheory61 <63194523+HybridTheory61@users.noreply.github.com>
2021-01-28 17:04:12 +0530

    updated our package to latest version,removed and fixed an vulnerablities

--- src/App.js
+++ src/App.js
@@ -9,33 +9,30 @@ class App extends Component {
     this.state = {
       monsters: [],
       searchField: "",
+      title: "",
     };
   }
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
-  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
+
+  onSearchChange = (e) => {
+    this.setState({ searchField: e.target.value, title: e.target.value });
+  };
+
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+    const { monsters, searchField, title } = this.state;
 
     //monster.name.toLowerCase().include(searchField.toLowerCase())
 
     return (
       <div className="App">
-        <h1>Monster Rolodex</h1>
-        <SearchBox
-          placeholder="search monsters"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters}></CardList>
+        <h1>{title}</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={monsters}></CardList>
       </div>
     );
   }
